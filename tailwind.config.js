0dce7a370ad63d946c2c506c5936a11e942cfa2e
luisfejaramillor <luisfejaramillor@gmail.com>
2024-02-27 21:57:09 -0500

    updated config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,15 +6,10 @@ export default {
   ],
   theme: {
     fontSize: {
-      xs: ['12px', '16px'],
-      sm: ['14px', '20px'],
-      base: ['16px', '19.5px'],
-      lg: ['18px', '21.94px'],
-      xl: ['20px', '24.38px'],
-      '2xl': ['24px', '29.26px'],
-      '3xl': ['28px', '50px'],
-      '4xl': ['48px', '58px'],
-      '8xl': ['96px', '106px']
+      body: ['88px', '88px'],
+      m: ['48px', '56px'],
+      l: ['24px', '32px'],
+      xl: ['18px', '28px'],
     },
     extend: {
       fontFamily: {
@@ -22,11 +17,11 @@ export default {
         montserrat: ['Montserrat', 'sans-serif'],
       },
       colors: {
-        'primary': "#ECEEFF",
-        "coral-red": "#FF6452",
-        "slate-gray": "#6D6D6D",
-        "pale-blue": "#F5F6FF",
-        "white-400": "rgba(255, 255, 255, 0.80)"
+        'primary': "#151515",
+        "green": "#4EE1A0",
+        "dark-gray": "#242424",
+        "grey": "#D9D9D9",
+        "white": "#FFFFFF"
       },
       boxShadow: {
         '3xl': '0 10px 40px rgba(0, 0, 0, 0.1)'
